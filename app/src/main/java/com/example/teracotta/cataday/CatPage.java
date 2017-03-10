b05e90ac5d6b8d842f5ce3ae403a3bfb626dabab
Maja Gorzkowicz <maja.gorzkowicz@bbc.co.uk>
2017-03-10 09:14:21 +0000

    made basic connection with reddit to get 1st top post from reddit; still needs username and password, need to change that… hidden now

--- app/src/main/java/com/example/teracotta/cataday/CatPage.java
+++ app/src/main/java/com/example/teracotta/cataday/CatPage.java
@@ -3,9 +3,19 @@ package com.example.teracotta.cataday;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.paginators.SubredditPaginator;
 
 public class CatPage extends AppCompatActivity {
     private Cat redditCat;
@@ -43,6 +53,44 @@ public class CatPage extends AppCompatActivity {
     private Cat getTopRedditCat() {
         Cat topCat = new Cat("test title", "test author", "http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-4-57b30a939dff5__605.jpg");
         // here retrieve Cat object data
+
+
+
+        new Thread(new Runnable()
+        {
+            final String username = getString(R.string.username);
+            final String password = getString(R.string.password);
+            final String clientId = getString(R.string.client_id);
+            final String clientSecret = getString(R.string.secret);
+            final String appId = getString(R.string.app_id);
+
+            @Override
+            public void run()
+            {
+                UserAgent myUserAgent = UserAgent.of("android",
+                        appId, "0.0.1", username);
+                RedditClient redditClient = new RedditClient(myUserAgent);
+                Credentials credentials = Credentials.script(username,
+                        password, clientId, clientSecret);
+                try {
+                    OAuthData authData = redditClient.getOAuthHelper()
+                            .easyAuth(credentials);
+                    redditClient.authenticate(authData);
+                } catch (OAuthException e) {
+                    e.printStackTrace();
+                }
+
+                SubredditPaginator paginator
+                        = new SubredditPaginator(redditClient);
+                Listing<Submission> submissions = paginator.next();
+                final Submission first = submissions.get(0);
+
+                Log.d("first", first.getTitle());
+            }
+        }).start();
+
+
+
         return topCat;
     }
 }
