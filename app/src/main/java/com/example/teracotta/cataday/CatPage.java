e99b6c886b957e531eeeea83ad7a169adbfaaff1
Maja Gorzkowicz <maja.gorzkowicz@bbc.co.uk>
2017-03-22 15:08:58 +0000

    added favourite add/remove communication

--- app/src/main/java/com/example/teracotta/cataday/CatPage.java
+++ app/src/main/java/com/example/teracotta/cataday/CatPage.java
@@ -1,7 +1,9 @@
 package com.example.teracotta.cataday;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import net.dean.jraw.RedditClient;
@@ -27,6 +30,7 @@ import java.util.UUID;
 public class CatPage extends AppCompatActivity {
     Callback catCallback;
     Submission topSubmission;
+    static boolean isCatStillFavourite = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,10 @@ public class CatPage extends AppCompatActivity {
                         setTopCatValues(cat.getSubmissionImageURL(), cat.getSubmissionTitle(), cat.getSubmissionAuthor());
                         setShareButton(cat.getSubmissionLink());
                         setGotoButton(cat.getSubmissionLink());
+                        isCatStillFavourite = cat.getIsFavourite();
+                        cat.setIsFavourite(setFavouriteButton());
+                        setFavouritePageButton();
+
                         final LinearLayout loadingLayout = (LinearLayout) findViewById(R.id.loadingLayout);
                         loadingLayout.setVisibility(LinearLayout.GONE);
                     }
@@ -60,16 +68,6 @@ public class CatPage extends AppCompatActivity {
         authorLine.setText(submissionAuthor);
     }
 
-    private void setGotoButton(final String submissionLink) {
-        findViewById(R.id.goto_button).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent linkIntent = new Intent(Intent.ACTION_VIEW);
-                linkIntent.setData(Uri.parse(submissionLink));
-                startActivity(linkIntent);
-            }
-        });
-    }
-
     private void setShareButton(String submissionLink) {
         final String sharedMessage = "Check out the current top photo on reddit/r/cats! \n\n" + submissionLink;
         final Button shareButton = (Button) findViewById(R.id.share_button);
@@ -82,6 +80,57 @@ public class CatPage extends AppCompatActivity {
                 startActivity(Intent.createChooser(linkIntent, "Share via"));
             }
         });
+    }
+
+    private void setGotoButton(final String submissionLink) {
+        findViewById(R.id.goto_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent linkIntent = new Intent(Intent.ACTION_VIEW);
+                linkIntent.setData(Uri.parse(submissionLink));
+                startActivity(linkIntent);
+            }
+        });
+    }
+
+    private boolean setFavouriteButton() {
+        final Button favouriteButton = (Button) findViewById(R.id.add_to_favourites_button);
+        favouriteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(isCatStillFavourite) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(CatPage.this).create();
+                    alertDialog.setTitle("Warning");
+                    alertDialog.setMessage("Are you sure you want to remove this post from Favourites?");
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    favouriteButton.setText(R.string.add_to_favourites_button_text);
+                                    dialog.dismiss();
+                                    isCatStillFavourite = false;
+                                }
+                            });
+                    alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "CANCEL",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    isCatStillFavourite = true;
+                                }
+                            });
+                    alertDialog.show();
+                } else {
+                    Toast.makeText(CatPage.this, "The post was added to Favourites",
+                            Toast.LENGTH_LONG).show();
+                    isCatStillFavourite = true;
+                    favouriteButton.setText(R.string.remove_from_favourites_button_text);
+                }
+            }
+        });
+        return isCatStillFavourite;
+    }
+
+    private void setFavouritePageButton() {
+        // check if the cat is favourite
+            // check if it exists in json
+            // add if it doesn't
     }
 
     interface Callback {
